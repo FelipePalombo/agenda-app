0c6175f6b45d4fc6025dff37c367baa33349899c
Felipe Ribeiro <feliperibeiro.rp@gmail.com>
2019-04-24 21:49:33 -0300

    Ajustado component de formulário, utilizando alguns itens do Ant Design

--- src/Template/CenterContent/Tarefas.jsx
+++ src/Template/CenterContent/Tarefas.jsx
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
-import { Layout, Col, Row } from 'antd';
+import { Layout, Col, Row, Typography } from 'antd';
 import BreadCrumb from './BreadCrumb';
 import TodoForm from '../Todo/todo-form'
 import TodoList from '../Todo/todo-list'
+
+const { Title } = Typography;
 
 class Tarefas extends Component{
     render(){
@@ -14,13 +16,13 @@ class Tarefas extends Component{
                 <Row style={{minHeight: 800, background: '#fff'}}>
                     <Col span={40} style={{ padding: 24}}>
                         <Row type="flex" justify="center" align="center">
-                            <h1>Tarefas</h1>
+                            <Title>Tarefas</Title>
                         </Row>
-                        <Row type="flex" justify="center" align="center" style={{marginTop:'20px'}}>
-                            <TodoForm/>
+                        <Row style={{marginTop:'20px'}}>
+                            <TodoForm type="flex" justify="center" align="center"/>
                         </Row>    
                         <Row type="flex" justify="center" align="center" style={{marginTop:'20px'}}>
-                            <TodoList/>
+                            <TodoList />
                         </Row>    
                     </Col>
                 </Row>
--- src/Template/Todo/todo-form.jsx
+++ src/Template/Todo/todo-form.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Form, Icon, Input, Button, Layout, Col, Row } from 'antd';
 
-export default () => (
-    <div>
-        <h1>Form will be around here</h1>
-    </div>
-)
+class TodoAForm extends Component{     
+    constructor(props){
+        super(props);
+
+        this.type = this.props.type;
+        this.justify = this.props.justify;
+        this.align = this.props.align;
+    }
+
+    render(){
+        return(   
+            <Row type={this.type} justify={this.justify} align={this.align}>
+                <Col span={48}> 
+                    <Form layout="inline" onSubmit={this.handleSubmit} style={{ background: '#fff'}} >
+                        <Form.Item>
+                            <Input style={{ width: 600}} size="large" prefix={<Icon type="book" style={{ color: 'rgba(0,0,0,.25)'}} />} type="text" placeholder="Nova tarefa" />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button
+                                type="primary"
+                                htmlType="submit"
+                                icon="plus"
+                                shape="circle"
+                                size="large"
+                            >
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Col>    
+            </Row>
+            
+        )
+    }        
+}            
+
+export default TodoAForm;
